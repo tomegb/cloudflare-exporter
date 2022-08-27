0e6964be23aa040f501e57f5434f8faba5c56405
Mikka <dev@kxmpetentes.de>
2022-08-28 01:52:55 +0200

    Changes by tegb

--- cloudflare-exporter/src/main/java/com/github/yazui/cloudflare/exporter/Main.java
+++ cloudflare-exporter/src/main/java/com/github/yazui/cloudflare/exporter/Main.java
@@ -1,5 +1,10 @@
 package com.github.yazui.cloudflare.exporter;
 
+import com.github.yazui.cloudflare.exporter.api.CloudflareAccess;
+import com.github.yazui.cloudflare.exporter.api.requests.RequestRunner;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,8 +16,23 @@ public class Main {
                            "  \\___|_|\\___/ \\__,_|\\__,_|_| |_|\\__,_|_|  \\___|    \\___/_/\\_\\ .__/ \\___/|_|   \\__\\___|_|   \n" +
                            "                                                             | |                            \n" +
                            "                                                             |_|                            ");
-    }
 
+        Timer time = new Timer();
+        RequestRunner st = new RequestRunner(new CloudflareAccess(mail, key));
+        time.schedule(st, 0, TimeUnit.SECONDS.toMillis(30));
+    }
 }
 
 
+ /*
+   public void insertData() {
+    InfluxDBClient connection = new InfluxDBConnection("token", "bucket", "org", "url")
+        .buildConnection();
+
+    WriteApiBlocking writeApi = connection.getWriteApiBlocking();
+    Point point = Point.measurement("requests")
+        .addField("requests", "")
+        .time(Instant.now(), WritePrecision.MS);
+    writeApi.writePoint(point);
+  }
+  */
