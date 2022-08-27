79b55084efb6930a7c3e2c40d0f2829435d7a144
Mikka <dev@kxmpetentes.de>
2022-08-27 18:20:08 +0200

    Add own Cloudflare Api wrapper

---
+++ cloudflare-exporter/src/main/java/com/github/yazui/cloudflare/exporter/api/CloudflareZone.java
@@ -0,0 +1,13 @@
+package com.github.yazui.cloudflare.exporter.api;
+
+public class CloudflareZone extends CloudflareComponent {
+
+  //private final String name;
+  //private final String status;
+  //private final boolean paused;
+  //private final String type;
+
+  public CloudflareZone(String id) {
+    super(id);
+  }
+}
